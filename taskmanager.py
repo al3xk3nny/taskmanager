92e402d2820b47afde081da027ce312bcbc0690c
Alex Kenny <alexkenny1@gmail.com>
2018-10-26 09:31:16 +0000

    Did Mongo right

--- taskmanager.py
+++ taskmanager.py
@@ -9,20 +9,33 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app) # Creates an object called "mongo" that connects us to the Mongo database and which we use in the below code (i.e. tasks = mongo.db.tasks.find())
 
-@app.route("/")
-def show_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+# @app.route("/")
+# def get_tasks():
+#     tasks = mongo.db.tasks.find()
+#     return render_template("tasks.html", tasks=tasks)
     
-@app.route("/add_task", methods = ["GET", "POST"])
+@app.route("/tasks/<collection_name>")
+def get_tasks_by_collection(collection_name):
+    tasks = mongo.db[collection_name].find()
+    return render_template("tasks.html", tasks=tasks) 
+    
+    
+@app.route("/add_task", methods=["GET", "POST"])
 def add_task():
-    
-    if request.method == "POST":
-        mongo.db.tasks.insert_one(request.form.to_dict())
+    if request.method=="POST":
+        form_values = request.form.to_dict()
+        form_values["is_urgent"] = "is_urgent" in form_values
+        category = form_values["category_name"]
+        mongo.db[category].insert_one(form_values)
         return redirect("/")
-    else:    
-        categories = mongo.db.categories.find()
+    else:
+        categories = []
+        for category in mongo.db.collection_names():
+            if not category.startswith("system."):
+                categories.append(category)
+        
         return render_template("add_task.html", categories=categories)
+        
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
