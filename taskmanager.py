26d942432a8190e0a55d52064c64400921f9278a
Alex Kenny <alexkenny1@gmail.com>
2018-10-30 11:58:33 +0000

    Added categories

--- taskmanager.py
+++ taskmanager.py
@@ -20,13 +20,14 @@ def get_category_names():
 
 @app.route("/")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+    categories = get_category_names()
+    return render_template("tasks.html", categories=categories, category='Task List')
     
 @app.route("/tasks/<category>")
 def get_tasks_by_category(category):
     tasks = mongo.db[category].find()
-    return render_template("tasks.html", tasks=tasks) 
+    categories = get_category_names()
+    return render_template("tasks.html", tasks=tasks, categories=categories, category=category) 
     
     
 @app.route("/add_task", methods=["GET", "POST"])
@@ -39,29 +40,26 @@ def add_task():
         return redirect("/")
     else:
         categories = get_category_names()
-        
         return render_template("add_task.html", categories=categories)
         
 
-@app.route("/tasks/<category>/<task_id>/edit", methods=["GET", "POST"] )
+@app.route('/tasks/<category>/<task_id>/edit', methods=["GET", "POST"])
 def edit_task(category, task_id):
     if request.method=="POST":
         form_values = request.form.to_dict()
         form_values["is_urgent"] = "is_urgent" in form_values
         mongo.db[category].update({"_id": ObjectId(task_id)}, form_values)
         
-        if form_values["category_name"] != category: # If the "category_name" is still the same as the one present in the URL, simply run the update.
+        if form_values["category_name"] != category:
             the_task = mongo.db[category].find_one({"_id": ObjectId(task_id)})
             mongo.db[category].remove(the_task)
             mongo.db[form_values["category_name"]].insert(the_task)
             
-        return redirect(url_for("get_tasks_by_category", category=form_values["category_name"])) # Redirect goes to page task was moved to.
+        return redirect(url_for("get_tasks_by_category", category=form_values["category_name"]))
     else:
         the_task =  mongo.db[category].find_one({"_id": ObjectId(task_id)})
-        
         categories = get_category_names()
-        
-        return render_template("edit_task.html", task=the_task, categories=categories)
+        return render_template('edit_task.html', task=the_task, categories=categories)
 
 
 @app.route('/tasks/<category>/<task_id>/delete', methods=["POST"])
@@ -69,6 +67,19 @@ def delete_task(category, task_id):
     the_task = mongo.db[category].find_one({"_id": ObjectId(task_id)})
     mongo.db[category].remove(the_task)
     return redirect(url_for("get_tasks_by_category", category=category))
+
+
+@app.route('/categories')
+def get_categories():
+    categories = get_category_names()
+    return render_template("categories.html", categories=categories)
+
+
+@app.route('/categories/add', methods=["POST"])
+def add_category():
+    category_name = request.form["category_name"]
+    mongo.db.create_collection(category_name)
+    return redirect(url_for("get_categories"))
 
 
 if __name__ == "__main__":
